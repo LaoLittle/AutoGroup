c83c0de5d51ce21d208508260f1145c9ebf20869
LaoLittle <y1941803875@outlook.com>
2021-12-08 00:21:02 +0800

    add cacheclear

--- src/main/kotlin/AutoGroup.kt
+++ src/main/kotlin/AutoGroup.kt
@@ -22,7 +22,6 @@ import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.event.subscribeGroupMessages
 import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.utils.MiraiExperimentalApi
-import net.mamoe.mirai.utils.MiraiInternalApi
 import net.mamoe.mirai.utils.info
 import org.laolittle.plugin.joinorquit.AutoConfig.nudgeMin
 import org.laolittle.plugin.joinorquit.model.getPat
@@ -31,7 +30,6 @@ import java.time.LocalDateTime
 import java.util.*
 
 @ExperimentalSerializationApi
-@MiraiInternalApi
 @ConsoleExperimentalApi
 @ExperimentalCommandDescriptors
 @MiraiExperimentalApi
@@ -40,19 +38,20 @@ object AutoGroup : KotlinPlugin(
         id = "org.laolittle.plugin.AutoGroup",
         version = "1.0",
         name = "AutoGroup"
-    ){
+    ) {
         author("LaoLittle")
         info("折磨群友")
     }
 ) {
     override fun onEnable() {
         AutoConfig.reload()
+        var lastMessage: Map<Long, String> = mutableMapOf()
+        var onEnable: Map<Long, Boolean> = mutableMapOf()
         val nudgePerm = AutoGroup.registerPermission("timer.nudge", "每隔${nudgeMin}分钟戳一戳")
-        val tmp = File("$dataFolder/tmp")
-        if (tmp.exists()) tmp.deleteRecursively()
+
         logger.info { "开始折磨群友" }
         GlobalEventChannel.subscribeOnce<BotOnlineEvent> {
-            class NudgeTimer : TimerTask(){
+            class NudgeTimer : TimerTask() {
                 override fun run() {
                     logger.info { "开戳" }
                     this@AutoGroup.launch {
@@ -67,24 +66,29 @@ object AutoGroup : KotlinPlugin(
                     }
                 }
             }
+
             val nudgeTimer = NudgeTimer()
             Timer().schedule(nudgeTimer, Date(), nudgeMin * 60 * 1000)
-            GlobalEventChannel.subscribeOnce<BotOfflineEvent> { nudgeTimer.cancel() }
         }
+
         GlobalEventChannel.subscribeAlways<GroupTalkativeChangeEvent> {
             group.sendMessage(At(previous) + PlainText(" 的龙王被") + At(now) + PlainText(" 抢走了，好可怜"))
         }
+
         GlobalEventChannel.subscribeAlways<MemberJoinEvent> {
             group.sendMessage("欢淫")
             getPat(member, 80)
             group.sendImage(File("$dataFolder/tmp").resolve("${member.id}_pat.gif"))
         }
+
         GlobalEventChannel.subscribeAlways<MemberLeaveEvent.Kick> {
             group.sendMessage("有个人被$operator 踢了！！好可怕")
         }
+
         GlobalEventChannel.subscribeAlways<MemberLeaveEvent.Quit> {
             group.sendMessage("有个人悄悄退群了...")
         }
+
         GlobalEventChannel.subscribeAlways<MemberMuteEvent> {
             group.sendMessage(buildMessageChain {
                 add(At(member))
@@ -93,57 +97,70 @@ object AutoGroup : KotlinPlugin(
                 add(PlainText(" 禁言了，好可惜"))
             })
         }
+
         GlobalEventChannel.subscribeAlways<MemberUnmuteEvent> {
             group.sendMessage(buildMessageChain {
                 add(At(member))
                 add(" 你自由啦！还不快感谢")
-                add(At(operator!!))
-                add(" 大人")
+                if (operatorOrBot == group.botAsMember)
+                    add("我")
+                else {
+                    add(At(operatorOrBot))
+                    add(" 大人")
+                }
             })
         }
+
         GlobalEventChannel.subscribeAlways<BotMuteEvent> {
             try {
                 operator.sendMessage("就是你禁言的我吧")
                 delay(1000)
                 operator.sendMessage("咕姆姆，我记住你了")
-            }catch (e: Exception){
-                logger.error ( "$e 好像没法发送临时消息..." )
+            } catch (e: Exception) {
+                logger.error("$e 好像没法发送临时消息...")
             }
         }
-        GlobalEventChannel.subscribeGroupMessages {
-            startsWith("allinall") {
-                val replaced = it.replace("allinall", "")
-                if (replaced == "") return@startsWith
-                val msg = buildForwardMessage {
-                            val randomMember = subject.members.random()
-                            add(randomMember, PlainText(replaced))
-                }
-                subject.sendMessage(msg)
-            }
-        }
+
         GlobalEventChannel.subscribeAlways<BotUnmuteEvent> {
+            delay(1000)
             group.sendMessage(buildMessageChain {
                 add("我自由啦！感谢")
                 add(At(operator))
                 add(" 大人 🥵🥵🥵🥵🥵🥵🥵🥵")
             })
         }
+
+        GlobalEventChannel.subscribeGroupMessages {
+            startsWith("allinall") {
+                val replaced = it.replace("allinall", "")
+                if (replaced == "") return@startsWith
+                val msg = buildForwardMessage {
+                    val randomMember = subject.members.random()
+                    add(randomMember, PlainText(replaced))
+                }
+                subject.sendMessage(msg)
+            }
+        }
+
         GlobalEventChannel.subscribeAlways<BotJoinGroupEvent> {
             group.sendMessage("我来啦！！！")
         }
+
         GlobalEventChannel.subscribeAlways<GroupMuteAllEvent> {
-            if (!new){
+            if (!new) {
                 group.sendMessage("嗯？好像能说话了耶")
             }
         }
+
         GlobalEventChannel.subscribeAlways<MemberPermissionChangeEvent> {
             val msg = when {
                 origin.isOwner() || new.isOwner() -> PlainText("群主变了？？？")
                 origin.isAdministrator() && !new.isOperator() -> At(member).plus(PlainText(" 的管理没了，好可惜"))
-                else -> At(member).plus(PlainText(" 的管理没了，好可惜"))
+                else -> At(member).plus(PlainText(" 升职啦！"))
             }
             group.sendMessage(msg)
         }
+
         GlobalEventChannel.subscribeAlways<NudgeEvent> {
             if (target == bot) {
                 val msg = when ((0..5).random()) {
@@ -157,11 +174,12 @@ object AutoGroup : KotlinPlugin(
                         delay(1000)
                         try {
                             if (!from.nudge().sendTo(subject)) {
-                                subject.sendMessage("你以为关了戳一戳我就不能戳了么")
-                                delay(500)
+                                subject.sendMessage("戳不了...那我")
+                                delay(1000)
                                 subject.sendMessage(PokeMessage.ChuoYiChuo)
                             }
                         } catch (e: UnsupportedOperationException) {
+                            logger.info { "协议为不支持的协议，改用Poke戳一戳" }
                             subject.sendMessage(PokeMessage.ChuoYiChuo)
                         }
                         "哼"
@@ -171,15 +189,39 @@ object AutoGroup : KotlinPlugin(
                 subject.sendMessage(msg)
             }
         }
+
+        GlobalEventChannel.subscribeAlways<GroupMessageEvent> {
+            if (onEnable[group.id] == true) return@subscribeAlways
+            if (lastMessage[group.id] == message.serializeToMiraiCode()) {
+                onEnable = onEnable.plus(group.id to true)
+                subject.sendMessage(message)
+                logger.info { "复读了一次" }
+                this@AutoGroup.launch {
+                    delay(8_000)
+                    onEnable = onEnable.minus(group.id)
+                }
+            }
+            lastMessage = lastMessage.plus(group.id to message.serializeToMiraiCode())
+        }
+
+        Timer().schedule(CacheClear(), Date(), 60 * 30 * 1000)
     }
 
     override fun onDisable() {
         logger.info { "让他们休息会" }
     }
 
+    class CacheClear : TimerTask() {
+        override fun run() {
+            val tmp = File("${AutoGroup.dataFolder}/tmp")
+            when (if (tmp.exists()) tmp.deleteRecursively() else null) {
+                true -> AutoGroup.logger.info { "缓存清理完成" }
+                false -> AutoGroup.logger.info { "缓存清理失败" }
+            }
+        }
+    }
+
     private fun AbstractJvmPlugin.registerPermission(name: String, description: String): Permission {
         return PermissionService.INSTANCE.register(permissionId(name), description, parentPermission)
     }
-
-
 }
