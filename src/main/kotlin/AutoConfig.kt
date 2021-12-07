bdbf0168b755921df38d86e62e3ac3566b046c01
LaoLittle <y1941803875@outlook.com>
2021-12-08 00:20:34 +0800

    Config

--- src/main/kotlin/AutoConfig.kt
+++ src/main/kotlin/AutoConfig.kt
@@ -3,7 +3,6 @@ package org.laolittle.plugin.joinorquit
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
-import net.mamoe.mirai.utils.BotConfiguration
 
 object AutoConfig : AutoSavePluginConfig("AutoConfig") {
     @ValueDescription("戳一戳的时间间隔(单位：分)")
