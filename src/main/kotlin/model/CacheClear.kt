9f8985c3e8faa503d4a93bee106ebda723832846
LaoLittle <y1941803875@outlook.com>
2021-12-08 11:41:57 +0800

    add cacheClear

---
+++ src/main/kotlin/model/CacheClear.kt
@@ -0,0 +1,24 @@
+package org.laolittle.plugin.joinorquit.model
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.utils.MiraiExperimentalApi
+import net.mamoe.mirai.utils.info
+import org.laolittle.plugin.joinorquit.AutoGroup
+import java.io.File
+import java.util.*
+
+@MiraiExperimentalApi
+@ExperimentalCommandDescriptors
+@ConsoleExperimentalApi
+@ExperimentalSerializationApi
+class CacheClear : TimerTask() {
+    override fun run() {
+        val tmp = File("${AutoGroup.dataFolder}/tmp")
+        when (if (tmp.exists()) tmp.deleteRecursively() else null) {
+            true -> AutoGroup.logger.info { "缓存已自动清理" }
+            false -> AutoGroup.logger.info { "缓存清理失败" }
+        }
+    }
+}
